344638cc59288bea6acd92d041d20bb4ecff72e4
Amini-Philips <phillip_amini@yahoo.com>
2021-07-13 08:11:54 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -7,7 +7,7 @@
 const keys = {
   mongoDbURI: process.env.STAR_WARS_MOVIE_DB,
   sqlDbURI: process.env.STAR_WARS_FANS_DB,
-  serverPort: 5000 || process.env.APP_SERVER_PORT,
+  serverPort: process.env.PORT,
   dataSource: {
     films: process.env.FILM_DS,
     people: process.env.PEOPLE_DS,
