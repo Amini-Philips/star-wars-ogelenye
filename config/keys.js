2a008ff0fdffd3311e30b11d55bf551189c39ceb
Amini-Philips <phillip_amini@yahoo.com>
2021-07-13 01:31:57 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,17 +1,17 @@
 // This file contains all configuration settings
 
-import dotenv from "dotenv";
+//import dotenv from "dotenv";
 
-dotenv.config();
+//dotenv.config();
 
 const keys = {
   mongoDbURI: process.env.STAR_WARS_MOVIE_DB,
   sqlDbURI: process.env.STAR_WARS_FANS_DB,
-  serverPort: process.env.PORT,
+  serverPort: process.env.APP_SERVER_PORT,
   dataSource: {
     films: process.env.FILM_DS,
     people: process.env.PEOPLE_DS,
   },
 };
 
-export default keys;
+//export default keys;
